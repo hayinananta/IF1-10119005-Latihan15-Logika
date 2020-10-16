7f2ff06f649149014e00782ed46a1ef340903112
hayinanjay <72958200+hayinanjay@users.noreply.github.com>
2020-10-16 14:34:10 +0700

    Add files via upload

---
+++ src/pbo1/pkg10119005/latihan15/PBO110119005Latihan15.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo1.pkg10119005.latihan15;
+
+/**
+ *
+ * @author Hayin
+ * NAMA     : Hayin Ananta
+ * KELAS    : IF-1
+ * NIM      : 10119005
+ * Deskripsi Program : Logika
+ */
+public class PBO110119005Latihan15 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+        System.out.println("a && b = " + b);
+        System.out.println("a || b = " + a);
+        System.out.println("!a = " + b);
+        System.out.println("!(a && b) = " +  a) ;
+    }
+    
+}
